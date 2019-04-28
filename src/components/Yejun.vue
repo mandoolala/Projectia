43962043600773164a0229032beb6d1301b7da7d
hyperthreading <no1expman@gmail.com>
2019-04-28 22:46:31 +0900

    Yejun 's commit

---
+++ src/components/Yejun.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <p>
+            Yejun TODOs<br>
+            <span class="description">Press Enter to Add A New TODO</span>
+        </p>
+        <input v-model="input" v-on:keypress.enter="clearInput(); addTodo($event.target.value)"/>
+        <ul>
+            <li v-bind:key="todo.createdAt" v-for="todo in todos">
+                {{ todo.text }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Yejun",
+    data() {
+      return {
+        input: '',
+        todos: []
+      }
+    },
+    methods: {
+      clearInput () {
+        this.input = '';
+      },
+      addTodo: function(todo) {
+        this.todos.unshift({
+          text: todo,
+          createdAt: new Date().getTime()
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    .description {
+        color: red;
+    }
+</style>
