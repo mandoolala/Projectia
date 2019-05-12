e54e90185961c3b19040aaef551281d1a338ee85
hyperthreading <no1expman@gmail.com>
2019-05-12 16:05:04 +0900

    Add default branches and seeds
    Prettify App.vue

---
+++ src/constants.js
@@ -0,0 +1,62 @@
+export const defaultBranches = [
+  {
+    id: 1,
+    branch: "Euicheon-branch",
+    name: "Fix bugs in PullRequestList.vue",
+    isPulled: false,
+    isMerged: false,
+    collect_status:"Collect",
+    water_status: "Water",
+    reward: "cherry_blossom",
+    status_src: require("./assets/open.png"),
+    reward_src: require("./assets/rewards/cherryblossom.png")
+  },
+  {
+    id: 2,
+    branch:"Minjoo-branch",
+    name:"",
+    isPulled: false,
+    isMerged: false,
+    collect_status:"Collect",
+    water_status: "Water",
+    reward: "flower",
+    status_src: require("./assets/open.png"),
+    reward_src:require("./assets/rewards/dottoritree.png")
+  },
+  {
+    id: 3,
+    branch:"Minjoo-branch",
+    name:"",
+    isPulled: false,
+    isMerged: false,
+    collect_status:"Collect",
+    water_status: "Water",
+    reward: "cherry_blossom",
+    status_src: require("./assets/open.png"),
+    reward_src:require("./assets/rewards/dottoritree.png")
+  },
+  {
+    id: 4,
+    branch:"Minjoo-branch",
+    name:"",
+    isPulled: false,
+    isMerged: false,
+    collect_status:"Collect",
+    water_status: "Water",
+    reward: "dotori",
+    status_src: require("./assets/open.png"),
+    reward_src:require("./assets/rewards/dottoritree.png")
+  },
+  {
+    id: 5,
+    branch:"Minjoo-branch",
+    name:"",
+    isPulled: false,
+    isMerged: false,
+    collect_status:"Collect",
+    water_status: "Water",
+    reward: "dotori",
+    status_src: require("./assets/open.png"),
+    reward_src:require("./assets/rewards/dottoritree.png")
+  }
+];
